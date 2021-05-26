8a6b4dffb802533ed780bcfbe7cf21f1e44a1097
oceane <oceane.sailorin@live.com>
2021-05-27 00:11:47 +0200

    fibonacci function using recursion

--- fibo.py
+++ fibo.py
@@ -1,16 +1,20 @@
-#write function fib(n)
+# write function fibo(n) using recursion
 # take n number as an argument
 # return nth fibonacci number
-# fib(n) : 1, 1, 2, 3, 5, 8, 13, 21, 34
+# fibo(n) : 1, 1, 2, 3, 5, 8, 13,...
 # to remove O(2**n) complexity, we need to decrease number of recursive calls and reuse subcalculations
 
-def fib(n, emptyDict = {}):
-    if n in emptyDict: 
-        return emptyDict[n]
+def fibo(n, subDict = {}):
+    if n < 0: return "invalid number"
+    if n == 0: return 0
+    if n in subDict: 
+        return subDict[n]  
     if n <=2: 
         return 1
-    return fib(n-1) + fib(n-2)
+    subDict[n] = fibo(n-1,subDict) + fibo(n-2,subDict)
+    return subDict[n]
 
-print("\n",fib(6))
-print("\n",fib(7))
-print("\n",fib(8))
+
+print("\n",fibo(7))
+print("\n",fibo(8))
+print("\n",fibo(100))
