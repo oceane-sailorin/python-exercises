700617c7dc05fce06d7184e4b877a8785cc3cf37
oceane <oceane.sailorin@live.com>
2021-05-28 22:19:49 +0200

    trip function using recursion

--- trip.py
+++ trip.py
@@ -1,10 +1,17 @@
-# how many ways can you travel to the goal on a grid dimension m * n
+# how many ways can you travel to the goal on a grid dimension m * n using recursion
+# to remove O(2**m+n) complexity, we need to decrease number of recursive calls and reuse subresults
 
-def gridTravel(m,n):
+def gridTravel(m,n, subDict = {}):
+    tupArg = (m,n)
+    if tupArg in subDict: 
+        return subDict[tupArg]  
     if n==1 and m==1: return 1
     if n==0 or m==0 : return 0
-    return gridTravel(m - 1, n) + gridTravel(m, n - 1)
+    subDict[tupArg] =  gridTravel(m - 1, n, subDict) + gridTravel(m, n - 1, subDict)
+    return subDict[tupArg]
 
 
+print(gridTravel(0,3))
 print(gridTravel(2,3))
 print(gridTravel(6,3))
+print(gridTravel(21,24))
