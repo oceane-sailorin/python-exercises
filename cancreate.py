006ea91ef393cc671b31f29d13b9c4f9eb6c9263
oceane <oceane.sailorin@live.com>
2021-06-04 23:36:14 +0200

    add function cancreate

---
+++ cancreate.py
@@ -0,0 +1,12 @@
+# write function cancreate(target,words) with arguments target string and list of strings
+# function should return boolean whether it is possible to create target with words
+# one word can be used several times
+# use of recursion and memoization
+
+def canCreate(target, words):
+
+    return False
+
+print(canCreate("milestone",["mil","one","tone","est"]))   
+print(canCreate("autonomous",["nom","mous","aut","o","mo"]))   
+print(canCreate("careless",["ca","carl","cor","car","mo","s","ele"]))   
