a47d8c3575ee55e15a9c1720623cad6d8047127a
oceane <oceane.sailorin@live.com>
2021-06-22 22:50:32 +0200

    add madlib

---
+++ little_games/madlibs/madlibs.py
@@ -0,0 +1,17 @@
+#madlibs
+# string concatenation
+#madword = "mad"
+
+#print("concat with "+ madword)
+#rint("concat with {} ",format(madword))
+#print(f"concat with {madword} ")
+
+adj = input("Adjective: ")
+verb = input("Verb: ")
+verb2 = input("Can't Verb: ")
+name1 = input("Name: ")
+
+madlib = f"Machine learning is so {adj}! I've always wanted to learn how to program it because \
+I love to {verb}. I don't know what else I could do because I can't {verb2} like {name1}"
+
+print(madlib)
