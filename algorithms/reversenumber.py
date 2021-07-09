6258d13884ae48b0dbf87a643576357c8bfa8df8
oceane <oceane.sailorin@live.com>
2021-07-09 23:01:06 +0200

    updates

--- algorithms/reversenumber.py
+++ algorithms/reversenumber.py
@@ -2,12 +2,17 @@
 
 class Solution:
     def reverse(self, x: int) -> int:
-        if x >= 2**31-1 or x <= -2**31: return 0
-        if x < 0: return -1 * int(str(x*-1)[::-1])
-        else: return int(str(x)[::-1])
+        if x < 0: 
+            result = -1 * int(str(x*-1)[::-1])
+        else :
+            result = int(str(x)[::-1])
+        return 0 if result >= 2**31-1 or result <= -2**31 else result
 
 
 s1 = Solution()
 
 print(s1.reverse(-128454))
 print(s1.reverse(5874369))
+print(s1.reverse(2147483648))
+print(s1.reverse(1534236469))
+
