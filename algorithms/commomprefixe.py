2fef79abbb01aed9157a5433bf963c3a8a8f0ca4
oceane <oceane.sailorin@live.com>
2021-07-10 22:34:46 +0200

    add commonprefix

---
+++ algorithms/commomprefixe.py
@@ -0,0 +1,31 @@
+#commonprefixe
+
+class Solution:
+    def longestCommonPrefix(self, arr1) -> str:
+        
+        if not arr1 or len(arr1) > 200: return ""
+        
+        car = min(arr1, key=len)
+        if len(car) == 0: return ""
+        if len(max(arr1, key=len)) > 200: return ""
+
+        res = ""
+
+        for i, s in enumerate(car):
+            #print("s="+s)
+            for str in arr1 :
+                #print("stri="+str[i])
+                if str[i] != s:
+                    return res
+            res += s    
+        return res   
+
+
+s1 = Solution()
+
+print(s1.longestCommonPrefix(["flower","flow","flight"]))
+print(s1.longestCommonPrefix(["fine","finally","finess"]))
+print(s1.longestCommonPrefix(["correct","correctness","correctnessly"]))
+print(s1.longestCommonPrefix(["juilo","juil",""]))
+print(s1.longestCommonPrefix([]))
+print(s1.longestCommonPrefix(["jkjk","ioio","gygy","vgvg","cfcf","rtrt"]))
