8d4f520d73962bfea431b2997381aa78c8830a93
oceane <oceane.sailorin@live.com>
2021-10-06 23:39:32 +0200

    add is same tree

---
+++ algorithms/is_same_tree.py
@@ -0,0 +1,31 @@
+# issametree
+"""
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+"""
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left         
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if(p is None and q is None):
+            return True
+        elif(p is None or q is None):
+            return False
+
+        if(p.val==q.val):
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
+    
+
+s1 = Solution()
+s2 = TreeNode()
+
+print(s1.isSameTree([1,2,3], [1,2,3]))
