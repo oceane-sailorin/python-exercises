b109898ca21817f83228084fb515d477323fc232
oceane <oceane.sailorin@live.com>
2021-10-31 22:13:49 +0100

    add count unique digits

--- algorithms/count_numbers_with_unique_digits.py
+++ algorithms/count_numbers_with_unique_digits.py
@@ -8,11 +8,22 @@ Given an integer n, return the count of all numbers with unique digits, x, where
 class Solution:
     def countNumbersWithUniqueDigits(self, n: int) -> int:
         if (n == 0):
-            return 0
+            return 1
         if (n == 1):
             return 10
         if (n == 2):
             return 91    
+
+          
+        res = 91
+        countnum = [0, 10, 81]
+
+        for x in range(3,n +1):
+            #append countnum
+            countnum.append(countnum[x - 1] * (9 - x + 2))
+            res += countnum[x]
+   
+        return res
 
 
 s1 = Solution()
@@ -22,4 +33,8 @@ s1 = Solution()
 print(s1.countNumbersWithUniqueDigits(1))
 
 print(s1.countNumbersWithUniqueDigits(2))
+
+print(s1.countNumbersWithUniqueDigits(3))
+
+print(s1.countNumbersWithUniqueDigits(5))
 
