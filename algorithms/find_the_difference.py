fa2d453a1ed6f8a400342718a4e83ea520145eac
oceane <oceane.sailorin@live.com>
2021-11-06 21:56:39 +0100

    add find difference

--- algorithms/find_the_difference.py
+++ algorithms/find_the_difference.py
@@ -8,9 +8,12 @@ Return the letter that was added to t.
 
 """
 
+import operator
+import collections
+from functools import reduce
 
 class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
+    def findTheDifference2(self, s: str, t: str) -> str:
         s_set = set(s)
         t_set = set(t)
         print(s_set)
@@ -18,7 +21,18 @@ class Solution:
         diff = ''.join(s_set.symmetric_difference(t_set))
         return diff
 
-      
+    def findTheDifference3(self, s: str, t: str) -> str:
+        s_list = list(s)
+        t_list = list(t)
+        return chr(reduce(operator.xor, map(ord, s), 0) ^ reduce(operator.xor, map(ord, t), 0))
+
+    def findTheDifference(self, s: str, t: str) -> str:     
+        t_list = list(t)
+        s_list = list(s)
+        for i in s_list:
+            t_list.remove(i)
+        if t_list: return t_list[0]
+        else: return ""
     
 
 
