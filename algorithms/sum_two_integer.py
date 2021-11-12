53ec64afcf49972e3856e34634cb8f68f858788c
oceane <oceane.sailorin@live.com>
2021-11-12 22:51:34 +0100

    add two integers

--- algorithms/sum_two_integer.py
+++ algorithms/sum_two_integer.py
@@ -6,13 +6,13 @@ Given two integers a and b, return the sum of the two integers without using the
 
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        if not a and not b: return None
-        if not a or a == 0 : return b
-        if not b or b == 0: return a
+
+        if a == 0 : return b
+        if b == 0: return a
+        # find the sum if different bits with XOR (0 if same bits)
         c = a ^ b
+        # to find real sum when same bits, use & (AND) to find a carry then shift carry one position left and assign it to d
         d = (a & b) << 1
-        print(c)
-        print(d)
         return self.getSum(c, d)
 
 
@@ -22,10 +22,14 @@ s1 = Solution()
 
 print(s1.getSum(1, 2))
 
-#print(s1.getSum(8, 12))
+print(s1.getSum(2, 2))
 
-#print(s1.getSum(0, 0))
+print(s1.getSum(8, 12))
 
-#print(s1.getSum(1000,2000))
+print(s1.getSum(0, 0))
 
+print(s1.getSum(1000,2000))
 
+print(s1.getSum(-10,-20))
+
+print(s1.getSum(-10,20))
