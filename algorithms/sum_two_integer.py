69be9e96f58f09f9dde8619c3fe581c3df63ff9d
oceane <oceane.sailorin@live.com>
2021-11-13 17:15:47 +0100

    add two integers

--- algorithms/sum_two_integer.py
+++ algorithms/sum_two_integer.py
@@ -3,8 +3,10 @@
 """
 Given two integers a and b, return the sum of the two integers without using the operators + and -.
 """
+import math
 
 class Solution:
+    #solution for positive int
     def getSum(self, a: int, b: int) -> int:
 
         if a == 0 : return b
@@ -13,7 +15,29 @@ class Solution:
         c = a ^ b
         # to find real sum when same bits, use & (AND) to find a carry then shift carry one position left and assign it to d
         d = (a & b) << 1
+        
         return self.getSum(c, d)
+
+    #math solution
+    def getSum2(self, a: int, b: int) -> int:
+
+        return int(math.log2(2**a * 2**b))
+
+    #bitwise and mask solution from leetcode solutions
+    def getSum3(self, a: int, b: int) -> int:
+    
+        # 32 bits integer max
+        MAX = 0x7FFFFFFF
+        # 32 bits interger min
+        MIN = 0x80000000
+        # mask to get last 32 bits
+        mask = 0xFFFFFFFF
+        while b != 0:
+            # ^ get different bits and & gets double 1s, << moves carry
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        # if a is negative, get a's 32 bits complement positive first
+        # then get 32-bit positive's Python complement negative
+        return a if a <= MAX else ~(a ^ mask)
 
 
 s1 = Solution()
@@ -30,6 +54,34 @@ print(s1.getSum(0, 0))
 
 print(s1.getSum(1000,2000))
 
-print(s1.getSum(-10,-20))
+print("------------------")
 
-print(s1.getSum(-10,20))
+print(s1.getSum2(1, 2))
+
+print(s1.getSum2(2, 2))
+
+print(s1.getSum2(8, 12))
+
+print(s1.getSum2(0, 0))
+
+print(s1.getSum2(1000,2000))
+
+print(s1.getSum2(-10,-20))
+
+print(s1.getSum2(-10,20))
+
+print("------------------")
+
+print(s1.getSum3(1, 2))
+
+print(s1.getSum(32, 2))
+
+print(s1.getSum3(8, 12))
+
+print(s1.getSum3(0, 0))
+
+print(s1.getSum3(1000,2000))
+
+print(s1.getSum3(-10,-20))
+
+print(s1.getSum3(-10,20))
