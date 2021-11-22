99bbad5cb910fd0086500e52632729e874f2cd58
oceane <oceane.sailorin@live.com>
2021-11-22 20:31:43 +0100

    add other move zeroes

--- algorithms/movezeroes.py
+++ algorithms/movezeroes.py
@@ -15,17 +15,27 @@ class Solution:
 
         for i in range(nonzeroes,len(nums)):
             nums[i] = 0
+           
+        return nums
 
-        
-               
-        print(nums)
 
+    def moveZeroes2(self,nums):
+        for i in nums:
+            if i == 0:
+                nums.remove(i)
+                nums.append(i)
+        return nums
 
 
 s1 = Solution()
 
-s1.moveZeroes([0,1,0,3,12])
-s1.moveZeroes([0])
-s1.moveZeroes([0,5,9,4,2,0,7,6,12,18,23,27,48,59,65,47,21,58,62,35,98,952,987,548,124,587,632,8,0,2,4,9856,0,25,73,0,569,842,26])
-s1.moveZeroes([0,0,0,0,0,0,0,0,0,1])
-s1.moveZeroes([5,3,7,4,9,6,8,1,0,10,16,18,24,15,27])
+print(s1.moveZeroes([0,1,0,3,12]))
+print(s1.moveZeroes2([0,1,0,3,12]))
+print(s1.moveZeroes([0]))
+print(s1.moveZeroes2([0]))
+print(s1.moveZeroes([0,5,9,4,2,0,7,6,12,18,23,27,48,59,65,47,21,58,62,35,98,952,987,548,124,587,632,8,0,2,4,9856,0,25,73,0,569,842,26]))
+print(s1.moveZeroes2([0,5,9,4,2,0,7,6,12,18,23,27,48,59,65,47,21,58,62,35,98,952,987,548,124,587,632,8,0,2,4,9856,0,25,73,0,569,842,26]))
+print(s1.moveZeroes([0,0,0,0,0,0,0,0,0,1]))
+print(s1.moveZeroes2([0,0,0,0,0,0,0,0,0,1]))
+print(s1.moveZeroes([5,3,7,4,9,6,8,1,0,10,16,18,24,15,27]))
+print(s1.moveZeroes2([5,3,7,4,9,6,8,1,0,10,16,18,24,15,27]))
