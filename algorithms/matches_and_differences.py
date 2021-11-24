2001cd74059762653e99eb359a13060cc3ea3eb5
oceane <oceane.sailorin@live.com>
2021-11-24 19:19:26 +0100

    add matches and differences

---
+++ algorithms/matches_and_differences.py
@@ -0,0 +1,15 @@
+#Given two sentences, return an array with the words in common and the words that are different
+
+
+class Solution:
+    def matchesAndDifferences(self, string1, string2):
+        #change strings into sets
+        set1 = set(string1.split())
+        set2 = set(string2.split())
+        
+        return sorted(list(set1&set2)) ,sorted(list(set1^set2))
+
+s1 = Solution()
+
+print(s1.matchesAndDifferences("who do you think you are","whoever think you should be there"))
+print(s1.matchesAndDifferences("remind me if the numbers are correct","Correct this problem for me"))
