9be4c0e07634be5f7abbcbc151ef7c75860378b1
oceane <oceane.sailorin@live.com>
2021-11-27 18:12:23 +0100

    add find prime numbers

--- algorithms/find_prime_numbers.py
+++ algorithms/find_prime_numbers.py
@@ -17,22 +17,10 @@ class Solution:
                     res.append(n)
         return res
 
-    def findPrimeNumbers2(self, num):
-        numbers = range(3, num+1, 2)
-        half = (num)//2
-        initial = 4
-
-        for step in range(3, num+1, 2):
-            for i in range(initial, half, step):
-                numbers[i-1] = 0
-            initial += 2*(step+1)
-
-            if initial > half:
-                return [2] + filter(None, numbers)
-
 
 
 s1 = Solution()
 
 print(s1.findPrimeNumbers(5))
 print(s1.findPrimeNumbers(43))
+
