359003940ee113056708d177af972fd2a1bd0f6e
oceane <oceane.sailorin@live.com>
2021-12-04 22:22:11 +0100

    add binary_gap

--- lessons/binary_gap.py
+++ lessons/binary_gap.py
@@ -14,10 +14,23 @@ def binary_gap(num):
         return 0
     if (num < 1 or num > 2147483647):
         return 0
-        
-    b = "{0:b}".format(int(num))
 
-    return b
+    counter, temp = 0, 0
+    b = "{0:b}".format(int(num))
+    print(b)
+    for i in range(len(b)):
+        if int(b[i]) == 1:
+            if temp > counter:
+                counter = temp
+            temp = 0
+        elif int(b[i]) == 0:
+            temp += 1
+    
+    return counter
 
 
 print(binary_gap(5))
+
+print(binary_gap(529))
+
+print(binary_gap(32))
