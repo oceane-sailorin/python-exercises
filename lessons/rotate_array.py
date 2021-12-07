190cdb0e6fed3093e32c11d4b36026fd69c336a9
oceane <oceane.sailorin@live.com>
2021-12-07 22:53:59 +0100

    add rotation array

--- lessons/rotate_array.py
+++ lessons/rotate_array.py
@@ -5,9 +5,13 @@ which given an array and a number of time returns an array shifted K times to th
 """
 
 def rotate_array(arr1,num):
-    key = arr1[0]
-    key_index = 0
-    lst = arr1[:len(arr1)-num]
-    lst2 = arr1[:len(arr1)-num]
+    rotations = num % len(arr1)
+    return arr1[-rotations:] + arr1[:-rotations]
     
-       
+    
+print(rotate_array([3, 8, 9, 7, 6],3))   
+print(rotate_array([6, 3, 8, 9, 7],3))   
+print(rotate_array([0, 0, 0],3))   
+print(rotate_array([1, 2, 3, 4],4))   
+print(rotate_array([1,2],5))   
+print(rotate_array([6,8,10,-12,14,16,18,20,-22,24,-26,28,30,32,-34,36,40,42],7))   
