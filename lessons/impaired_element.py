7ebe51fbb6f76942325e9893e383e44ab4b01cf6
oceane <oceane.sailorin@live.com>
2021-12-09 19:54:56 +0100

    add impaired element

---
+++ lessons/impaired_element.py
@@ -0,0 +1,12 @@
+#impaired element
+
+"""Write a function 
+which given an array with an odd number 
+return the element which is impaired 
+"""
+
+def impaired_element(arr1):
+    return 0
+    
+    
+print(impaired_element([3, 8,3,7,8,8,3]))   
