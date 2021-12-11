0453465cff2b0514391e2368ff85770ccf9df3bc
oceane <oceane.sailorin@live.com>
2021-12-11 22:16:19 +0100

    add impaired element

--- lessons/impaired_element.py
+++ lessons/impaired_element.py
@@ -18,9 +18,22 @@ def impaired_element(arr1):
             return k
 
     return None
+
+def impaired_element2(arr1):
+
+    for i in arr1:
+        res = arr1.count(i)
+        if res == 1:
+            return i
+    return None
     
     
 print(impaired_element([3, 8,3,7,8,8,3]))   
 print(impaired_element([0,1,1]))   
 print(impaired_element([1]))   
 print(impaired_element([6,7,6,7,8,9,8,9,2,3,2,3,1,4,10,4,10]))   
+
+print(impaired_element2([3, 8,3,7,8,8,3]))   
+print(impaired_element2([0,1,1]))   
+print(impaired_element2([1]))   
+print(impaired_element2([6,7,6,7,8,9,8,9,2,3,2,3,1,4,10,4,10]))   
