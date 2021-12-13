d4b13da32b14f95056c0c608ca7504bd45394da6
oceane <oceane.sailorin@live.com>
2021-12-13 22:33:19 +0100

    add frog jump

--- lessons/frog_jump.py
+++ lessons/frog_jump.py
@@ -19,8 +19,24 @@ def frog_jump(X, Y, D):
 
     return jump
 
+def frog_jump2(X, Y, D):
+    #nbr jumps
+    if Y <= X:
+        return 0
+
+    jump = (Y - X) // D
+
+    if (Y - X) % D > 0:
+        return jump + 1
+
+    return jump
 
 print(frog_jump(10,85,30))
 print(frog_jump(0,-10,2))
 print(frog_jump(20,200,30))
 print(frog_jump(123,45874523,584))
+
+print(frog_jump2(10,85,30))
+print(frog_jump2(0,-10,2))
+print(frog_jump2(20,200,30))
+print(frog_jump2(123,45874523,584))
