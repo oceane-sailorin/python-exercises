49a2b5c569e17aa27ca505c1cead0860c1e06619
oceane <oceane.sailorin@live.com>
2022-01-01 23:02:48 +0100

    add perm check

---
+++ lessons/perm_check.py
@@ -0,0 +1,22 @@
+#perm check
+
+def perm_check(arr1):
+    #if start with 1
+    maximum = max(arr1)
+    expected_sum = sum(range(maximum+1))
+    arr1_sum = sum(set(arr1))
+    return int(expected_sum) == int(arr1_sum)
+ 
+
+
+print(perm_check([2,3,1,5]))
+
+print(perm_check([2,3,1,4]))
+
+print(perm_check([2,2,2,4]))
+
+print(perm_check([4,1,3,2]))
+
+print(perm_check([4,1,3]))
+
+print(perm_check([9,5,1,8,4,7,6,2,3,10,12,14,16,18,11,13,15,17,19]))
