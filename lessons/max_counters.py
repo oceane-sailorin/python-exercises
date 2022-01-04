56d27e3efd297e3c76610e0d6697b1bf1eba9e66
oceane <oceane.sailorin@live.com>
2022-01-04 16:54:32 +0100

    add max counters

---
+++ lessons/max_counters.py
@@ -0,0 +1,40 @@
+#max counters
+
+"""
+given an integer N and a non-empty array A consisting of M integers, returns a sequence of integers representing the values of the counters.
+
+Result array should be returned as an array of integers.
+"""
+
+def increase(X):
+    return X+1
+
+
+def max_counter(arr1):
+    maximum = max(arr1)
+    arr2 = [maximum] * (len(arr1))
+    return arr2
+
+def solution(N,A):
+    res = [0] * (N)
+    for x in A:
+        if x >= 1 and x <= N:
+            res[x - 1] = increase(res[x - 1])
+        elif x == N + 1 :
+            res = max_counter(res)
+    return res
+
+
+print(solution(5,[3,4,4,6,1,4,4]))
+
+print(solution(5,[0]))
+
+print(solution(5,[2,2,2]))
+
+print(solution(5,[6,6,6]))
+
+print(solution(5,[1,6,6]))
+
+print(solution(5,[5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6]))
+
+print(solution(500,list(range(1, 5000))))
