86f6cfcc4645f9b446c91b8900e62ae4916ef6c5
oceane <oceane.sailorin@live.com>
2022-01-06 22:43:45 +0100

    add missing element

---
+++ lessons/missing_integer.py
@@ -0,0 +1,33 @@
+# missing integer
+"""
+given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A
+"""
+
+def solution(A):
+    smallest = 1
+    latest = 0
+    if not A: return 1
+    sorted_arr = sorted(set(A))
+    for id, val in enumerate(sorted_arr):
+        if id > 0:
+            smallest = max(sorted_arr[id - 1] + 1, 1)
+            if val > 0 and val > smallest:
+                return smallest
+        latest = val
+    if latest > 0: return smallest + 1
+    return smallest
+
+
+
+
+print(solution([1, 3, 6, 4, 1, 2]))
+
+print(solution([-1, -3]))
+
+print(solution([1, 2, 3]))
+
+print(solution([-8,-6,5,-6,-2,-4,-6,-4,7]))
+
+print(solution([]))
+
+print(solution([-8,-6,5,-6,-2,-4,-6,-4,7,1]))
