dfca105972671db7948d8830cdfc057296cbf51e
oceane <oceane.sailorin@live.com>
2022-01-21 19:51:06 +0100

    add max product of three

--- lessons/max_product_of_three.py
+++ lessons/max_product_of_three.py
@@ -40,12 +40,52 @@ Write an efficient algorithm for the following assumptions:
         N is an integer within the range [3..100,000];
         each element of array A is an integer within the range [−1,000..1,000].
 """
-
-def solution(A):
+#with sorting
+def solution2(A):
     A.sort()
     return max(A[0]*A[1]*A[-1], A[-1]*A[-2]*A[-3])
 
+#without sorting
+#multiply 3 biggest numbers or the 2 smallest (to take into account multiplication of 2 negative numbers) with the biggest
+def solution(A):
 
+    #initialize 3 biggest numbers with min - 1 values
+    max1 = -1001
+    max2 = -1001
+    max3 = -1001
 
+    #initialize 2 smallest numbers with max + 1 values
+    min1 = 1001
+    min2 = 1001
+
+    n = len(A)
+    for i in range(0,n):
+        #we decide max1 is the biggest, max2 second biggest and max3 third biggest
+        if A[i] > max1:
+            #A[i] is now the biggest so we report the value of max2 to max3, max1 to max2 and A[i] to max1
+            max3 = max2
+            max2 = max1
+            max1 = A[i]
+        elif A[i] > max2:
+            #A[i] is now the second biggest so we report the value of max2 to max3 and A[i] to max2
+            max3 = max2
+            max2 = A[i]
+        elif A[i] > max3:
+            #A[i] is now the third biggest so we report the value of A[i] to max3  
+            max3 = A[i]
+
+        #we decide min1 is the smallest, min2 is the second smallest and now we find if A[i] is a min
+        if A[i] < min1:
+            #A[i] is now the smallest so we report the value of min1 to min2 and A[i] to min1
+            min2 = min1
+            min1 = A[i]
+        elif A[i] < min2:
+            #A[i] is the second smallest so we report the value of A[i] to min2
+            min2 = A[i]
+
+    #at the end of the iteration, we have the 3 biggest and the 2 smallest
+    return max(min1 * min2 * max1, max1 * max2 * max3)
+
+     
 
 print(solution([-3,1,2,-2,5,6]))
