d58a87863c77fb77f88662b70f8317e0411ddbc6
oceane <oceane.sailorin@live.com>
2022-01-26 19:56:57 +0100

    add number of discs intersections

--- lessons/number_of_discs_intersections.py
+++ lessons/number_of_discs_intersections.py
@@ -30,39 +30,45 @@ The function should return −1 if the number of intersecting pairs exceeds 10,0
 Given array A shown above, the function should return 11, as explained above.
 """
 
+
 def solution(A):
     N = len(A)
     if N <2:
         return 0
-    res = 0
-    result = []
-    dict = {}
+    all = []
+    #we are going to move from 2D into 1D, along the x axis 
+    # and enter and go out of circles from the smallest value to the biggest 
+    # and count the intersections along the way
     for i in range(N):
-        for j in range(A[i]+1):
-            if i+j in dict: 
-                dict[i+j].append(i)
-                res += 1
-            else:
-                dict[i+j] = [i]
-                
-            
-            if j > 0:
-                if i-j in dict:
-                    dict[i-j].append(i)
-                    res += 1
-                else:
-                    dict[i-j] = [i]
-                
-            if res > 10000000: 
-                return -1
+        #add begin points into all with reference to begin
+        all.append([i-A[i],"b"])
+        #add end points into all with reference to end
+        all.append([i+A[i],"e"])
+    
+    #sort by first index of value then by second to keep begin values ahead of same end values (b before e)
+    all.sort(key=lambda k: (k[0], k[1]))
+    in_circle = 0
+    prev_circle = 0
+    res = 0
+    #print(all)
+    #when we enter a circle, we add the number of circles in which we are
+    #if we are in more than one circle, we add the corresponding intersections = number of circles we had before : each of them must be paired with the new one
+    #when we move out of a circle, we remove one from the number of circles and we update the previous circles value
+    for i in all:
+        if i[1] == "b":
+            in_circle += 1
+            if in_circle > 1:
+                res += prev_circle
+                #print("ic="+ str(i) + " "+ str(in_circle)+ " "+ str(prev_circle) + " "+ str(res))
+        elif i[1] == "e":
+            if in_circle > 0:
+                in_circle -= 1
+        prev_circle = in_circle
+        if res > 10000000: 
+            return -1
 
-    for k,v in dict.items():
-        if len(v) > 1 :
-            v = list(set(v))
-            result.extend([(a, b) for idx, a in enumerate(v) for b in v[idx + 1:]])
-
-    result = list(set(result))
-    return len(result)
+    return res
+   
 
 print(solution([1,5,2,1,4,0]))
 
@@ -72,7 +78,12 @@ print(solution([1]))
 
 print(solution([25,58,54,754,321,28,278,221,136]))
 
-
 print(solution([0,1]))
+
 print(solution([0, 0]))
+
 print(solution([1,0,0,3]))
+
+print(solution([4587,1263,2,145,25639,456985,4523,1,145896,5,856922,145,253214,14236,4442,666,523,3,25478,4,0,2,365,0,5478,6,1458887]))
+
+#complexity O(NlogN) and O(N)
