e0fddd92c00e0050d103e5a7ada56ca90658e875
oceane <oceane.sailorin@live.com>
2022-01-27 21:02:06 +0100

    add brackets

---
+++ algorithms/brackets.py
@@ -0,0 +1,37 @@
+#brackets
+"""
+A string S consisting of N characters is considered to be properly nested if any of the following conditions is true:
+
+        S is empty;
+        S has the form "(U)" or "[U]" or "{U}" where U is a properly nested string;
+        S has the form "VW" where V and W are properly nested strings.
+
+For example, the string "{[()()]}" is properly nested but "([)()]" is not.
+
+Given a string S consisting of N characters, return 1 if S is properly nested and 0 otherwise.
+
+For example, given S = "{[()()]}", the function should return 1 and given S = "([)()]", 
+the function should return 0, as explained above.
+
+"""
+
+def solution(S):
+    dict = {"]":"[", "}":"{", ")":"("}
+    li = ["]","[", "}","{", ")","("]
+    res = []
+    for car in S:
+        if car not in li:
+            return False
+            
+        if car in dict.keys():
+            if not res or dict[car] != res.pop(): 
+                return False
+        elif car in dict.values():
+            res.append(car)
+    return len(res) == 0
+
+
+
+print(solution("{[()()]}"))
+
+print(solution("([)()]"))
