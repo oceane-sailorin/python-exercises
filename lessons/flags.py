9ee9da07b2fd6d5cf3b0f72b241ebb507098facf
oceane <oceane.sailorin@live.com>
2022-03-06 15:25:29 +0100

    add flags

--- lessons/flags.py
+++ lessons/flags.py
@@ -123,3 +123,15 @@ def solution(A):
 
 
 print(solution([1,5,3,4,3,4,1,2,3,4,6,2]))
+
+print(solution([]))
+
+print(solution([1,1,1]))
+
+print(solution([1]))
+
+print(solution([1,9,5,7,6,3,4,2,8,5,1,2,3,6,7,2,8,1,9,3,5,6,4,3,9,8,3,7,2,8,1,2,3,6,8,7,2,5,6,3,6,5,3,9,5,3,8,3,8]))
+
+print(solution([2,2,2,2,8,2,2,2,2,8,2]))
+
+print(solution([2,8,8,2,8,8,2,8]))
