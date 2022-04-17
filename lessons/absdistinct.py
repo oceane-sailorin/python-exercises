3e7442fa01ada2376b81b936f2a35f84ca85091e
oceane <oceane.sailorin@live.com>
2022-04-17 17:19:55 +0200

    add absdistinct

--- lessons/absdistinct.py
+++ lessons/absdistinct.py
@@ -1,6 +1,7 @@
 #absdistinct
 """
-A non-empty array A consisting of N numbers is given. The array is sorted in non-decreasing order. The absolute distinct count of this array is the number of distinct absolute values among the elements of the array.
+A non-empty array A consisting of N numbers is given. The array is sorted in non-decreasing order. 
+The absolute distinct count of this array is the number of distinct absolute values among the elements of the array.
 
 For example, consider array A such that:
   A[0] = -5
@@ -18,18 +19,13 @@ Write a function:
 
 that, given a non-empty array A consisting of N numbers, returns absolute distinct count of array A.
 
-For example, given array A such that:
-  A[0] = -5
-  A[1] = -3
-  A[2] = -1
-  A[3] =  0
-  A[4] =  3
-  A[5] =  6
-
-the function should return 5, as explained above.
-    """
+"""
 def solution(A):
-    return len(set([abs(x) for x in A]))
+    return len(set([abs(a) for a in A]))
 
-    
+def solution2(A):
+    return len(set([a*a for a in A]))
+
+
 print(solution([-5,-3,-1,0,3,6]))
+print(solution2([-5,-3,-1,0,3,6]))
