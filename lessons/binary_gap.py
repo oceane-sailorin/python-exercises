2d303fd466084f1c62da62eb0b8b7b21e98f39c9
oceane <oceane.sailorin@live.com>
2022-04-30 20:43:11 +0200

    add binary_gap

--- lessons/binary_gap.py
+++ lessons/binary_gap.py
@@ -7,19 +7,24 @@ or 0 if no binary gap
 
 def binary_gap(num):
 
+    #manage outbound cases
     if not isinstance(num, int):
         return 0
     if (num < 1 or num > 2147483647):
         return 0
 
     counter, temp = 0, 0
+    #write number in binary
     b = "{0:b}".format(int(num))
     print(b)
+    #count number of 0 between 1
     for i in range(len(b)):
+        #when 1 store value if greater then former stored value and reset
         if int(b[i]) == 1:
             if temp > counter:
                 counter = temp
             temp = 0
+        #when 0 increase counter
         elif int(b[i]) == 0:
             temp += 1
     
