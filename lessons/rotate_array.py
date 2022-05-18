bb96229ca0438766272dd66b4e72482b912c60cd
oceane <oceane.sailorin@live.com>
2022-05-18 22:22:12 +0200

    rotate_array

--- lessons/rotate_array.py
+++ lessons/rotate_array.py
@@ -1,7 +1,15 @@
 #rotate array to the right K time
 
-"""Write a function 
-which given an array and a number of time returns an array shifted K times to the right
+"""
+An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
+
+The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
+
+Write a function:
+
+    def solution(A, K)
+
+that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
 """
 
 def rotate_array(arr1,num):
