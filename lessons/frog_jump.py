3ddd47e3263bdcda7517db162f3eeed63bfe1e18
oceane <oceane.sailorin@live.com>
2022-05-19 22:47:39 +0200

    frog_jump

--- lessons/frog_jump.py
+++ lessons/frog_jump.py
@@ -1,7 +1,12 @@
 #frog jump
 
 """
-given 3 integers, count minimal number of jumps D from position X to Y or greater than Y. 
+A small frog wants to get to the other side of the road. 
+The frog is currently located at position X and wants to get to a position greater than or equal to Y. 
+The small frog always jumps a fixed distance, D.
+Count the minimal number of jumps that the small frog must perform to reach its target.
+Write a function:
+that, given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to or greater than Y.
 """
 
 def frog_jump(X, Y, D):
