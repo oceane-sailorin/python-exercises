43297f6c3f2259e945c3f357e93d3f85d14a36d7
oceane <oceane.sailorin@live.com>
2022-05-20 18:00:15 +0200

    perm_missing_element

--- lessons/perm_missing_element.py
+++ lessons/perm_missing_element.py
@@ -1,7 +1,20 @@
 #permutation missing element
 
 """
-given an array of integers n the range [1..(N + 1)], return the missing element
+An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+Your goal is to find that missing element.
+
+Write a function:
+that, given an array A, returns the value of the missing element.
+
+For example, given array A such that:
+  A[0] = 2
+  A[1] = 3
+  A[2] = 1
+  A[3] = 5
+
+the function should return 4, as it is the missing element.
 """
 
 def perm_missing_element(arr1):
