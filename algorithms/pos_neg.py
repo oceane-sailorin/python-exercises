34d6b1d37a1b1c2056b252446b9b7f4857e5498e
oceane <oceane.sailorin@live.com>
2022-07-21 22:54:18 +0200

    add pos_neg

---
+++ algorithms/pos_neg.py
@@ -0,0 +1,10 @@
+#Given 2 int values, return True if one is negative and one is positive. 
+# Except if the parameter "negative" is True, then return True only if both are negative.
+
+def pos_neg(a, b, negative):
+    return True if (a*b < 0 and negative==False) or (a<0 and b<0 and negative == True)  else False
+
+
+print(pos_neg(1, -1, False))
+print(pos_neg(-1, 1, False))
+print(pos_neg(-4, -5, True))
