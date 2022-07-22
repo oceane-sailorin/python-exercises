0000bf39741d37b6d917e69ad2414f0e7538a918
oceane <oceane.sailorin@live.com>
2022-07-22 17:24:56 +0200

    add pos_neg

--- algorithms/pos_neg.py
+++ algorithms/pos_neg.py
@@ -8,3 +8,20 @@ def pos_neg(a, b, negative):
 print(pos_neg(1, -1, False))
 print(pos_neg(-1, 1, False))
 print(pos_neg(-4, -5, True))
+print(pos_neg(1, -1, False)) #→ True	
+print(pos_neg(-1, 1, False)) #→ True	
+print(pos_neg(-4, -5, True)) #→ True	
+print(pos_neg(-4, 5, True)) #→ False	
+print(pos_neg(1, 1, False)) #→ False	
+print(pos_neg(-1, -1, False))#→ False	
+print(pos_neg(1, -1, True)) #→ False	
+print(pos_neg(-1, 1, True)) # → False		
+print(pos_neg(1, 1, True)) #→ False	
+print(pos_neg(-1, -1, True)) #→ True		
+print(pos_neg(5, -5, False)) #→ True		
+print(pos_neg(-6, 6, False)) #→ True	
+print(pos_neg(-5, -6, False)) #→ False	
+print(pos_neg(-2, -1, False)) #→ False		
+print(pos_neg(1, 2, False)) #→ False	
+print(pos_neg(-5, 6, True)) #→ False	
+print(pos_neg(-5, -5, True))#→ True	
