1597ee1f6c76379703391690675de8ce4c3fee09
oceane <oceane.sailorin@live.com>
2022-09-04 21:17:12 +0200

    add common_end

---
+++ algorithms/common_end.py
@@ -0,0 +1,11 @@
+#Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element.
+#  Both arrays will be length 1 or more.
+
+def common_end(a, b):
+    return a[-1:] == b[-1:] or a[:1] == b[:1]
+
+
+print(common_end([1, 2, 3], [7, 3]))
+print(common_end([1, 2, 3], [7, 3, 2]) )
+print(common_end([1, 2, 3], [1, 3]))
+print(common_end([1, 2, 3], [1]))
