8d08ac245bf14f9a8668a508625dffe0dfac159a
oceane <oceane.sailorin@live.com>
2022-09-25 22:18:04 +0200

    add sorta_sum

--- algorithms/sorta_sum.py
+++ algorithms/sorta_sum.py
@@ -7,3 +7,9 @@ def sorta_sum(a, b):
 print(sorta_sum(3, 4)) # → 7
 print(sorta_sum(9, 4)) # → 20
 print(sorta_sum(10, 11)) # → 21
+print(sorta_sum(12, -3)) # → 9	9	OK	
+print(sorta_sum(-3, 12)) # → 9	9	OK	
+print(sorta_sum(4, 5)) # → 9	9	OK	
+print(sorta_sum(4, 6)) # → 20	20	OK	
+print(sorta_sum(14, 7)) # → 21	21	OK	
+print(sorta_sum(14, 6)) # → 20	20	OK
