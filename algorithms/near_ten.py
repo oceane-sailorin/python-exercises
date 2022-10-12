056f1e0dcd618231ac9f20b09b92648b987e5e71
oceane <oceane.sailorin@live.com>
2022-10-12 20:23:42 +0200

    add near_ten

--- algorithms/near_ten.py
+++ algorithms/near_ten.py
@@ -8,3 +8,15 @@ def near_ten(num):
 print(near_ten(12)) # → True
 print(near_ten(17)) # → False
 print(near_ten(19)) # → True
+print(near_ten(31)) # → True	True	
+print(near_ten(6)) # → False	False	
+print(near_ten(10)) # → True	True		
+print(near_ten(11)) # → True	True	
+print(near_ten(21)) # → True	True	
+print(near_ten(22)) # → True	True	
+print(near_ten(23)) # → False	False	
+print(near_ten(54)) # → False	False	
+print(near_ten(155)) # → False	False	
+print(near_ten(158)) # → True	True	
+print(near_ten(3)) # → False	False	
+print(near_ten(1)) # → True	True
