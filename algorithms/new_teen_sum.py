3b2c8b66fb0262268dc1401383b18bc1162a484a
oceane <oceane.sailorin@live.com>
2022-10-19 19:11:50 +0200

    add no teen sum

--- algorithms/new_teen_sum.py
+++ algorithms/new_teen_sum.py
@@ -16,3 +16,16 @@ def no_teen_sum(a, b, c):
 print(no_teen_sum(1, 2, 3)) # → 6
 print(no_teen_sum(2, 13, 1)) # → 3
 print(no_teen_sum(2, 1, 14)) # → 3
+print(no_teen_sum(2, 1, 15) ) #→ 18	18	OK	
+print(no_teen_sum(2, 1, 16)) # → 19	19	OK	
+print(no_teen_sum(2, 1, 17)) # → 3	3	OK	
+print(no_teen_sum(17, 1, 2)) # → 3	3	OK	
+print(no_teen_sum(2, 15, 2) ) #→ 19	19	OK	
+print(no_teen_sum(16, 17, 18)) # → 16	16	OK	
+print(no_teen_sum(17, 18, 19)) #→ 0	0	OK	
+print(no_teen_sum(15, 16, 1) ) #→ 32	32	OK	
+print(no_teen_sum(15, 15, 19)) # → 30	30	OK	
+print(no_teen_sum(15, 19, 16)) # → 31	31	OK	
+print(no_teen_sum(5, 17, 18)) # → 5	5	OK	
+print(no_teen_sum(17, 18, 16)) # → 16	16	OK	
+print(no_teen_sum(17, 19, 18)) # → 0
