c2a856df5cc39b6d7a7c0d865f9ea0627b3b0e60
oceane <oceane.sailorin@live.com>
2022-11-12 21:11:43 +0100

    add close_far

---
+++ algorithms/close_far.py
@@ -0,0 +1,22 @@
+#Given three ints, a b c, return True if one of b or c is "close" (differing from a by at most 1), 
+# while the other is "far", differing from both other values by 2 or more. 
+# Note: abs(num) computes the absolute value of a number.
+def far(a,b):
+    if abs(b - a) >= 2:
+        return True
+    return False
+
+def close(a,b):
+    if abs(b - a) <= 1:
+        return True
+    return False
+
+def close_far(a, b, c):
+    if (far(a,b) and far(b,c) and close(a,c)) or (far(a,c) and far(b,c) and close(a,b)):
+        return True
+    return False
+
+
+print(close_far(1, 2, 10)) # → True
+print(close_far(1, 2, 3)) # → False
+print(close_far(4, 1, 3)) # → True
